b7164c005a9331fe7745d745e9aaa9da0b0bd1b4
legendkartik45 <ksrtik.khuda.45@gmail.com>
2025-02-16 17:34:28 +0530

    initialize project with Vue 3 and Vite setup

---
+++ src/components/FirstPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Welcome to First Page</h1>
+        <button @click="logout">Logout</button>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const logout = () => {
+    router.push('/');
+};
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="login-container">
+        <h2>Login</h2>
+        <input type="text" v-model="username" placeholder="Username" />
+        <input type="password" v-model="password" placeholder="Password" />
+        <button @click="login">Login</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const username = ref('');
+const password = ref('');
+const router = useRouter();
+
+const login = () => {
+    if (username.value && password.value) {
+        router.push('/first');
+    } else {
+        alert('Please enter credentials');
+    }
+};
+</script>
+
+<style scoped>
+.login-container {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    margin: 50px auto;
+}
+
+input,
+button {
+    margin: 5px 0;
+    padding: 8px;
+}
+</style>
